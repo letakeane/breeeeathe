031bf5f4e7dab27e933394facec8507db9556ef5
Leta Keane <leta@hey.com>
2023-11-15 11:45:55 -0700

    Adjust size of control panel

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -5,8 +5,15 @@ function Controls() {
   return (
     <section id="controls">
       <h2>Controls</h2>
-      <p className="credit">Ui icons created by <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Icon mania</a> - Flaticon</p>
-      <p className="credit">Settings icons created by<a href="https://www.flaticon.com/free-icons/settings" title="settings icons">dmitri13</a> - Flaticon</p>
+      <p>Something here</p>
+      <p>Something here</p>
+      <p>Something here</p>
+      <p>Something here</p>
+      <p>Something here</p>
+      <div id="credits">
+        <p className="credit">Ui icons created by <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Icon mania</a> - Flaticon</p>
+        <p className="credit">Settings icons created by<a href="https://www.flaticon.com/free-icons/settings" title="settings icons">dmitri13</a> - Flaticon</p>
+      </div>
     </section>
   )
 }
